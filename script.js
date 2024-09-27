58424aa85b2a90ed679fb6e16d90d8bbd71d85c1
Divyanshu. <divyanshunikhade7482@gmail.com>
2024-09-27 18:38:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Jai Shree Ram
 
-Shery.imageEffect("#back", { 
-  style: 5, 
-   debug: true,
-  gooey: true 
-});
+
+Shery.imageEffect(".back", {
+    style: 5,
+    gooey: true,
+    config: {"a":{"value":2,"range":[0,30]},"b":{"value":-0.98,"range":[-1,1]},"aspect":{"value":2.089227386421023},"gooey":{"value":true},"infiniteGooey":{"value":true},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.4,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.58,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
+
+})
